6decb650f8c3e86662f1c00270b488ea687500b5
Waleed A. Afifi <waleed-afifi@windowslive.com>
2020-04-30 15:30:32 +0300

    add new js file and folder js/my-script.js

---
+++ js/my-script.js
@@ -0,0 +1,23 @@
+'user-strict';
+
+
+var name = prompt("Please enter your name", "Waleed");
+document.getElementById('Username').innerHTML  = name + " This is " ;
+
+function getCatINformationFromUser() {
+    var color = prompt("Type your fav color", "white");
+    var age = prompt("Type the age you need", "1");
+    var name = prompt("Type name you want to register with", "felfel");
+
+    if (age <= 1) {
+        age = age + " Kitty";
+    } else {
+        age = age + " Adult";
+    }
+
+    document.getElementById('user-needs').innerHTML = "<table><tr><td>name</td><td>"+ name +"</td></tr><tr><td>age</td><td>"+ age +"</td></tr><tr><td>color</td><td>"+ color +"</td></tr></table>";
+}
+
+function confirmFunction() {
+    confirm("Press a button!");
+}
